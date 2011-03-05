6bcf2daa95c04c20404939d940b7424bf41fa87a
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2011-03-04 23:22:56 -0500

    Fixed lesson 1

--- koans/lesson1-observablestreams.js
+++ koans/lesson1-observablestreams.js
@@ -23,33 +23,33 @@ Array.prototype.toObservable = function() {
 };
  
 test('SimpleSubscription', function() {
-    Rx.Observable.Return(42).Subscribe(function(x) { equals(x, 42); });	
+    Rx.Observable.Return(42).Subscribe(function(x) { equals(x, ___); });	
 });
 
 test('SimpleReturn', function() {
     var received = '';
     Rx.Observable.Return('Foo').Subscribe(function(x) { received = x; });
-    equals('Foo', received);
+    equals(___, received);
 });
 
 test('TheLastEvent', function() {
     var received = '';
     var numbers = ['Foo','Bar'];
     Rx.Observable.FromArray(numbers).Subscribe(function(x) { received = x; });
-    equals('Bar', received);
+    equals(___, received);
 });
 
 test('EveryThingCounts', function() {
 	var received = 0;
 	var numbers = [3, 4 ];
 	Rx.Observable.FromArray(numbers).Subscribe(function(x) { received += x; });
-	equals(7, received);	
+	equals(___, received);	
 });
 
 test('DoingInTheMiddle', function() {
 	var status = [];
 	var daysTillTest = Range.create(1, 4).reverse().toObservable();
-	daysTillTest.Do(function(d) { status.push(d + '=' + (d === 1 ? 'Study Like Mad' : 'Party')); }).Subscribe();
+	daysTillTest.Do(function(d) { status.push(d + '=' + (d === 1 ? 'Study Like Mad' : ___)); }).Subscribe();
 	equals('4=Party,3=Party,2=Party,1=Study Like Mad', status.toString());
 });
 
@@ -58,6 +58,6 @@ test('NothingListensUntilYouSubscribe', function() {
 	var numbers = Range.create(1,10).toObservable();
 	var observable = numbers.Do(function(n) { sum += n; });
 	equals(0, sum);
-	observable.Subscribe();
+	observable.___();
 	equals(10*11/2,sum);	
 });
