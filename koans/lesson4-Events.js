7ab8dd66e411879da1f5050f419062271316ccc8
Ryan Riley <ryan.riley@panesofglass.org>
2011-04-07 21:31:44 -0700

    Added an additional test for demonstrating that event subscriptions are selective by type.

--- koans/lesson4-Events.js
+++ koans/lesson4-Events.js
@@ -11,3 +11,15 @@ test('listening to events', function() {
 	
 	equals(received, ___);
 });
+
+test('listening to the right events', function() {
+	var received = '';
+	var subscription = $(document).toObservable('foo').Subscribe(function(e) { received += e.payload; });
+	$(document).trigger({ type: 'foo', payload : 'M'});
+	$(document).trigger({ type: 'bar', payload : 'A'});
+	$(document).trigger({ type: 'foo', payload : 'T'});
+	$(document).trigger({ type: 'foo', payload : 'T'});
+	subscription.Dispose();
+	
+	equals(received, ___);
+});
